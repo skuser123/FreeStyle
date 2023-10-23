54551116f75c884ada73fd887fe1e1f0a0675a2a
skuser123 <117560077+skuser123@users.noreply.github.com>
2023-10-23 15:35:37 +0530

    Add files via upload

---
+++ agar.java
@@ -0,0 +1,10 @@
+public class Example {
+    public static void printHello() {
+        System.out.println("this is freestyle project");
+    }
+
+    public static void main(String[] args) {
+        // Call the function
+        printHello();
+    }
+}
